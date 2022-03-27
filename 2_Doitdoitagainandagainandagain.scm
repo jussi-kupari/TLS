67a79baf9a8ace69faf152b2e341dfbe75b2651b
Jussi Kupari <jkupari@KI-CND1457CS2.user.ki.se>
2022-03-27 14:32:17 +0200

    Finished chapter two

--- 2_Doitdoitagainandagainandagain.scm
+++ 2_Doitdoitagainandagainandagain.scm
@@ -37,21 +37,33 @@
       ingredients. Go on to the next question. 
       Good luck."
 
-      Below is my attempt, called list-of-atoms?, using some techniques not introduced yet. |#
+      Below are two of my attempts, called list-of-atoms? and
+      list-of-atoms2? (very similar), using some techniques not introduced yet. |#
 
 ;; list-of-atoms? : List-of-Anything -> Boolean
 ;; Given list, produces true if it only contains atoms
 (define (list-of-atoms? l)
   (cond
-    [(null? l) true]
-    [else (if (atom? (car l))
+    ((null? l) #t)
+    (else (if (atom? (car l))
               (list-of-atoms? (cdr l))
-              false)]))
+              #f))))
+
+(define (list-of-atoms2? l)
+  (cond
+    ((null? l) #t)
+    (else (and (atom? (car l))
+               (list-of-atoms2? (cdr l))))))
 
 (list-of-atoms? '(Jack Sprat could eat no chicken fat)) ; ==> #t
 (list-of-atoms? '((Jack) Sprat could eat no chicken fat)) ; ==> #f
 (list-of-atoms? '(Jack (Sprat could) eat no chicken fat)) ; ==> #f
 (list-of-atoms? '()) ; ==> #t
+
+(list-of-atoms2? '(Jack Sprat could eat no chicken fat)) ; ==> #t
+(list-of-atoms2? '((Jack) Sprat could eat no chicken fat)) ; ==> #f
+(list-of-atoms2? '(Jack (Sprat could) eat no chicken fat)) ; ==> #f
+(list-of-atoms2? '()) ; ==> #t
 
 
 
@@ -285,36 +297,329 @@
 #| Q: Is it true or false that a is a member of lat where a is tea and lat is (coffee tea or milk) |#
 #| A: True, b/c tea is an expression in the list (coffee tea or milk) |#
 
-#| Q: |#
-#| A: |#
+#| Q: Is (member? a lat) true or false where a is poached and lat is (fried eggs and scrambled eggs) |#
+#| A: False, b/c poached is not an atom in the list (fried eggs and scrambled eggs) |#
 
-#| Q: |#
-#| A: |#
+;; member? : Atom LAT -> Boolean
+;; Given atom and lat, produces true if atom is found in the lat
+(define member? 
+  (λ (a lat) 
+    (cond 
+      ((null? lat) #f) 
+      (else (or (eq? (car lat) a) 
+                (member? a (cdr lat))))))) 
 
-#| Q: |#
-#| A: |#
+#| Q: What is the value of (member? a lat) where a is meat and lat is (mashed potatoes and meat gravy) |#
+#| A: True, b/c the atom meat is found in the lat (mashed potatoes and meat gravy) |#
+(member? 'meat '(mashed potatoes and meat gravy)) ; ==> #t
 
-#| Q: |#
-#| A: |#
+#| Q: How do we determine the value #t for the above application? |#
+#| A: We first ask if the lat is null. If it is not null, we ask if either the first element
+      in the list is the one we look for (returning #t) or repeating the application from the beginning
+      with the remaining list returns #t |#
 
-#| Q: |#
-#| A: |#
-
-#| Q: |#
-#| A: |#
-
-#| Q: |#
-#| A: |#
-
-#| Q: |#
-#| A: |#
-
-#| Q: |#
-#| A: |#
-
-#| Q: |#
-#| A: |#
+#| Q: What is the first question asked by (member? a lat) |#
+#| A: (null? lat) |#
 
 
 
+#|          *** The First Commandment ***
+                     (preliminary) 
+                    Always ask null?
+          as the first question in expressing 
+                     any function.                   |#
 
+
+
+#| Q: What is the meaning of the line ((null? lat) #f) where lat is (mashed potatoes and meat gravy) |#
+#| A: This line asks if the list l is null. If it is null, the application returns #f. If l is not
+      null, then the function proceeds to the next question. In this case (null? l) is #f and we
+      continue on to the next question |#
+(null? '(mashed potatoes and meat gravy)) ; ==> #f
+
+#| Q: What is the next question? |#
+#| A: else |#
+
+#| Q: Why is else the next question? |#
+#| A: All other needed questions are containded inside the following else question. |#
+
+#| Q: Is else really a question? |#
+#| A: Yes, apparently it is. It is a question that always returns true |#
+
+#| Q: What is the meaning of the line
+     (else (or (eq? (car lat) a) 
+               (member? a (cdr lat)))) |#
+#| A: This line asks if the first expression in lat is the same as the atom we look for or
+      if the atom is a member of the remaining part of the list |#
+
+#| Q: True or false: 
+       (or (eq? (car lat) a) 
+           (member? a  (cdr lat)))
+      where a is meat and lat is (mashed potatoes and meat gravy) |#
+#| A: True, b/c meat is an expression in the list (mashed potatoes and meat gravy) |#
+(or (eq? (car '(mashed potatoes and meat gravy)) 'meat) 
+    (member? 'meat  (cdr '(mashed potatoes and meat gravy)))) ; ==> #t
+
+#| Q: Is (eq? (car lat) a) true or false where a is meat and lat is (mashed potatoes and meat gravy) |#
+#| A: False, b/c mashed is not the same as meat |#
+(car '(mashed potatoes and meat gravy)) ; ==> 'mashed
+(eq? 'mashed 'meat) ; ==> #f
+(eq? (car '(mashed potatoes and meat gravy)) 'meat) ; ==> #f
+
+#| Q: What is the second question of (or ...) |#
+#| A: (member? a (cdr lat)), which asks if a is a member of the remaining part of lat |#
+
+#| Q: Now what are the arguments of member? |#
+#| A: a and (cdr lat), which are meat and (potatoes and meat gravy) |#
+
+#| Q: What is the next question |#
+#| A: (null? lat) * rememeber the first commandment * |#
+
+#| Q: Is (null? lat) true or false where lat is (potatoes and meat gravy) |#
+#| A: False, b/c (potatoes and meat gravy) is not an null list |#
+
+#| Q: What do we do now |#
+#| A: Ask the next question |#
+
+#| Q: What is the next question |#
+#| A: else |#
+
+#| Q: What is the meaning of
+      (or (eq? (car lat) a) 
+          (member? a (cdr lat)))|#
+#| A: This is to find out if the first expression in the list is the atom we are looking for
+      or if the atom is an expression in the remaining part of the list |#
+
+#| Q: Is a eq? to the car of lat |#
+#| A: False, b/c meat does not equal potatoes |#
+
+#| Q: So what do we do next? |#
+#| A: We ask the next question, which is: is meat a member of (and meat gravy)?
+      (member? meat (and meat gravy)) |#
+
+#| Q: What is the next question? |#
+#| A: (null? lat) |#
+
+#| Q: What do we do now? |#
+#| A: (null? lat) returns #f, so we continue to the next question |#
+
+#| Q: What is the next question? |#
+#| A: else |#
+
+#| Q: What is the value of
+      (or (eq? (car lat) a) 
+          (member? a (cdr lat))) |#
+#| A: because (eq? (car (and meat gravy)) meat) returns #f, the value becomes
+      the value of (member? meat (cdr (and meat gravy))) |#
+
+#| Q: Why? |#
+#| A: See above. B/c (eq? (car (and meat gravy)) meat) returns #f |#
+(eq? (car '(and meat gravy)) 'meat) ; ==> #f
+
+#| Q: What do we do now? |#
+#| A: Run member? with new arguments - Recur |#
+
+#| Q: What are the new arguments? |#
+#| A: meat and (meat gravy) |#
+
+#| Q: What is the next question? |#
+#| A: (null? lat) |#
+
+#| Q: What do we do now? |#
+#| A: (null? (meat gravy)) returns #f, so we continue to the next question |#
+
+#| Q: What is the next question? |#
+#| A: else |#
+
+#| Q: What is the value of 
+      (or (eq? (car lat) a) 
+          (member? a (cdr lat))) |#
+#| A: True, b/c (eq? (car (meat gravy) meat) returns #t |#
+ (car '(meat gravy)) ; ==> 'meat
+ (eq? 'meat 'meat) ; ==> #t
+
+#| Q: What is the value of the application (member? a lat) where a is meat and lat is (meat gravy) |#
+#| A: True, because meat is a member of the list (meat gravy) |#
+
+#| Q: What is the value of the application (member? a lat) where a is meat and lat is (and meat gravy) |#
+#| A: True, because meat is a member of the list (and meat gravy) |#
+
+#| Q: What is the value of the application (member? a lat) where a is meat and
+      lat is (potates and meat gravy) |#
+#| A: True, because meat is a member of the list (potatoes and meat gravy) |#
+
+#| Q: What is the value of the application (member? a lat) where a is meat and
+      lat is (mashed potates and meat gravy) |#
+#| A: True, because meat is a member of the list (mashed potatoes and meat gravy).
+      Note that this is our original lat. |#
+
+#| Q: Just to make sure you have it right, let's quickly run through it again.
+      What is the value of the application (member? a lat) where a is meat and
+      lat is (mashed potates and meat gravy) |#
+#| A: True |#
+
+#| Q: (null? lat) |#
+#| A: False. Move to the next question. |#
+
+#| Q: else|#
+#| A: True |#
+
+#| Q: (or (eq? (car lat) a) 
+          (member? a (cdr lat))) |#
+#| A: Let's see one expression at a time |#
+
+#| Q: (eq? (car lat) a) |#
+#| A: False. Go to the next question. |#
+
+#| Q: What next? |#
+#| A: Recur with a and (cdr lat) where a is meat and (cdr lat) is (potatoes and meat gravy). |#
+
+#| Q: (null? lat) |#
+#| A: False. Move to the next question. |#
+
+#| Q: else |#
+#| A: True, but b/c (eq? (car lat) a) returns #f, we recur member? again with new arguments
+      where a is meat and (cdr lat) is (and meat gravy). |#
+
+#| Q: (null? lat) |#
+#| A: False. Move to the next question. |#
+
+#| Q: else |#
+#| A: True, but b/c (eq? (car lat) a) returns #f, we recur member? again with new arguments
+      where a is meat and (cdr lat) is (meat gravy). |#
+
+#| Q: (null? lat) |#
+#| A: False. Move to the next question. |#
+
+#| Q: (eq? (car lat) a) |#
+#| A: True |#
+(eq? (car '(meat gravy)) 'meat) ; ==> #t
+
+#| Q: (or (eq? (car lat) a) 
+          (member? a (cdr lat))) |#
+#| A: True |#
+
+#| Q: What is the value of (member? a lat) where a is meat and lat is (meat gravy) |#
+#| A: True |#
+
+#| Q: What is the value of (member? a lat) where a is meat and lat is (and meat gravy) |#
+#| A: True |#
+
+#| Q: What is the value of (member? a lat) where a is meat and lat is (potatoes and meat gravy) |#
+#| A: True |#
+
+#| Q: What is the value of (member? a lat) where a is meat and lat is (mashed potatoes and meat gravy) |#
+#| A: True |#
+
+#| Q: What is the value of (member? a lat) where a is liver and lat is (bagels and lox) |#
+#| A: False |#
+(member? 'liver '(bagels and lox)) ; ==> #f
+
+#| Q: Let's work out why it is #f. What's the first question member? asks?|#
+#| A: (null? lat) |#
+
+#| Q: (null? lat) |#
+#| A: (null? (bagels and lox)) is #f, so we continue to the next question |#
+(null? '(bagels and lox)) ; ==> #f
+
+#| Q: else |#
+#| A: True, but (eq? (car lat) a) is #f so we recur with (member? a (cdr lat))
+      where a is liver and lat in (cdr lat) is (and lox) |#
+(eq? (car '(bagels and lox)) 'liver) ; ==> #f
+
+#| Q: (null? lat) |#
+#| A: (null? (and lox)) is #f, so we continue to the next question |#
+(null? '(and lox))  ; ==> #f
+
+#| Q: else |#
+#| A: True, but (eq? (car lat) a) is #f so we recur with (member? a (cdr lat))
+      where a is liver and lat in (cdr lat) is (lox) |#
+(eq? (car '(and lox)) 'liver) ; ==> #f
+
+#| Q: (null? lat) |#
+#| A: (null? (lox)) is #f, so we continue to the next question |#
+(null? '(lox)) ; ==> #f
+
+#| Q: else |#
+#| A: True, but (eq? (car lat) a) is #f so we recur with (member? a (cdr lat))
+      where a is liver and lat in (cdr lat) is () |#
+(eq? (car '(lox)) 'liver) ; ==> #f
+
+#| Q: (null? lat) |#
+#| A: (null? ()) is #t, so the application returns #f |#
+(null? '()) ; ==> #t
+
+#| Q: What is the value of (member? a lat) where a is liver and lat is () |#
+#| A: False |#
+(member? 'liver '()) ; ==> #f
+
+#| Q: What is the value of 
+      (or (eq? (car lat) a) 
+          (member? a (cdr lat))) 
+      where a is liver and lat is (lox) |#
+#| A: False |#
+(or (eq? (car '(lox)) 'liver)
+    (member? 'liver (cdr '(lox)))) ; ==> #f
+
+#| Q: What is the value of (member? a lat) where a is liver and lat is (lox) |#
+#| A: False |#
+(member? 'liver '(lox)) ; ==> #f
+
+#| Q: What is the value of
+      (or (eq? (car lat) a) 
+          (member? a (cdr lat)))
+      where a is liver and lat is (and lox) |#
+#| A: False |#
+(or (eq? (car '(and lox)) 'liver)
+    (member? 'liver (cdr '(and lox)))) ; ==> #f
+
+#| Q: What is the value of (member? a lat) where a is liver and lat is (and lox) |#
+#| A: False |#
+(member? 'liver '(and lox)) ; ==> #f
+
+#| Q: What is the value of
+      (or (eq? (car lat) a) 
+          (member? a (cdr lat)))
+      where a is liver and lat is (bagels and lox) |#
+#| A: False |#
+(or (eq? (car '(bagels and lox)) 'liver)
+    (member? 'liver (cdr '(bagels and lox)))) ; ==> #f
+
+#| Q: What is the value of (member? a lat) where a is liver and lat is (bagels and lox) |#
+#| A: False |#
+(member? 'liver '(bagels and lox)) ; ==> #f
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#|          *** This space for doodling ***          |#
